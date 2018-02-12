38e337131c47bb1d3b95ed63efda784daec8cb0a
Aleksbgbg <kozichka01@abv.bg>
2018-02-12 23:28:48 +0000

    Add tab switching command to ShellView with gesture Ctrl+Tab.

--- Fidl/ViewModels/ShellViewModel.cs
+++ Fidl/ViewModels/ShellViewModel.cs
@@ -1,16 +1,23 @@
 ﻿namespace Fidl.ViewModels
 {
+    using System.Windows.Input;
+
+    using Fidl.Helpers;
     using Fidl.ViewModels.Interfaces;
 
     internal sealed class ShellViewModel : ViewModelBase, IShellViewModel
     {
-        public ShellViewModel(IMainViewModel mainViewModel)
+        public ShellViewModel(IMainViewModel mainViewModel, ITabConductorViewModel tabConductorViewModel)
         {
             DisplayName = "Fidl";
 
             MainViewModel = mainViewModel;
+
+            SwitchTabCommand = new RelayCommand<object>(parameter => tabConductorViewModel.SwitchTab());
         }
 
         public IMainViewModel MainViewModel { get; }
+
+        public ICommand SwitchTabCommand { get; }
     }
 }
