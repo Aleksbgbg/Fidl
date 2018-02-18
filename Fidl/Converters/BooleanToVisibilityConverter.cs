2e10596e3f97929cb98dd853e68125f2a16acd0f
Aleksbgbg <kozichka01@abv.bg>
2018-02-18 18:25:32 +0000

    Add BooleanToVisibilityConverter

---
+++ Fidl/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace Fidl.Converters
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    internal class BooleanToVisibilityConverter : IValueConverter
+    {
+        public static BooleanToVisibilityConverter Instance { get; } = new BooleanToVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(value != null, $"{nameof(value)} != null");
+
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(value != null, $"{nameof(value)} != null");
+
+            return (Visibility)value == Visibility.Visible;
+        }
+    }
+}
