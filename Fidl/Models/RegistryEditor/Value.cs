71d3c3d699d6de21823f0ee82a217d42597d4a9f
Aleksbgbg <kozichka01@abv.bg>
2018-02-22 13:06:32 +0000

    Add initial Value model class to represent registry values.

---
+++ Fidl/Models/RegistryEditor/Value.cs
@@ -0,0 +1,20 @@
+﻿namespace Fidl.Models.RegistryEditor
+{
+    using Microsoft.Win32;
+
+    internal class Value
+    {
+        internal Value(RegistryKey registryKey, string name)
+        {
+            Name = name;
+            StoredValue = registryKey.GetValue(name);
+            Kind = registryKey.GetValueKind(name);
+        }
+
+        public string Name { get; }
+
+        public object StoredValue { get; }
+
+        public RegistryValueKind Kind { get; }
+    }
+}
