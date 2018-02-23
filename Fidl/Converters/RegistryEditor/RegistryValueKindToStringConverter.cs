4661bce6be93103cf113ad3809b1b3528b854bc5
Aleksbgbg <kozichka01@abv.bg>
2018-02-23 09:02:21 +0000

    Add converter to display RegistryValueKind correctly.

---
+++ Fidl/Converters/RegistryEditor/RegistryValueKindToStringConverter.cs
@@ -0,0 +1,54 @@
+﻿namespace Fidl.Converters.RegistryEditor
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Windows.Data;
+    using Microsoft.Win32;
+
+    [ValueConversion(typeof(RegistryValueKind), typeof(string))]
+    internal class RegistryValueKindToStringConverter : IValueConverter
+    {
+        public static RegistryValueKindToStringConverter Instance { get; } = new RegistryValueKindToStringConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(value != null, $"{nameof(value)} != null");
+
+            switch ((RegistryValueKind)value)
+            {
+                case RegistryValueKind.String:
+                    return "String (REG_SZ)";
+
+                case RegistryValueKind.ExpandString:
+                    return "ExpandString (REG_EXPAND_SZ)";
+
+                case RegistryValueKind.Binary:
+                    return "Binary (REG_BINARY)";
+
+                case RegistryValueKind.DWord:
+                    return "DoubleWord (REG_DWORD)";
+
+                case RegistryValueKind.MultiString:
+                    return "Multi-String (REG_MULTI_SZ)";
+
+                case RegistryValueKind.QWord:
+                    return "QuadWord (REG_QWORD)";
+
+                case RegistryValueKind.Unknown:
+                    return "Unknown (REG_UNKNOWN)";
+
+                case RegistryValueKind.None:
+                    return "None (REG_NONE)";
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported.");
+        }
+    }
+}
