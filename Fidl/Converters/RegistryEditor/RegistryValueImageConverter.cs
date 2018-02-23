4d398fdd3f5594c112064baef812ae8a11bea0a5
Aleksbgbg <kozichka01@abv.bg>
2018-02-23 21:32:24 +0000

    Added registry value images.

---
+++ Fidl/Converters/RegistryEditor/RegistryValueImageConverter.cs
@@ -0,0 +1,42 @@
+﻿namespace Fidl.Converters.RegistryEditor
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media.Imaging;
+
+    using Microsoft.Win32;
+
+    [ValueConversion(typeof(RegistryValueKind), typeof(BitmapSource))]
+    internal class RegistryValueImageConverter : IValueConverter
+    {
+        public static RegistryValueImageConverter Instance { get; } = new RegistryValueImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((RegistryValueKind)value)
+            {
+                case RegistryValueKind.String:
+                case RegistryValueKind.ExpandString:
+                case RegistryValueKind.MultiString:
+                    return Application.Current.FindResource("RegistryString");
+
+                case RegistryValueKind.Binary:
+                case RegistryValueKind.DWord:
+                case RegistryValueKind.QWord:
+                case RegistryValueKind.Unknown:
+                case RegistryValueKind.None:
+                    return Application.Current.FindResource("RegistryBinary");
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "RegistryValueKind is not valid.");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported.");
+        }
+    }
+}
